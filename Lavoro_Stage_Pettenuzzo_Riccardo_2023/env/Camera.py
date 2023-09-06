5ca9b9f3a7bc5c75b0b8aea578b9983cbd5c9ac4
RPette <118899569+RPette@users.noreply.github.com>
2023-09-06 17:43:14 +0200

    Start Documentation

--- Lavoro_Stage_Pettenuzzo_Riccardo_2023/env/Camera.py
+++ Lavoro_Stage_Pettenuzzo_Riccardo_2023/env/Camera.py
@@ -1,29 +1,22 @@
 import cv2
-from matplotlib import pyplot as plt
 
-cap = cv2.VideoCapture(1)
+#define the camera that would be used using an index 
+cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
 
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 3856)
-
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 2764)
-
-available_backends = [cv2.videoio_registry.getBackendName(i) for i in cv2.videoio_registry.getBackends()]
-
+#define variable for the codec
 fourcc = cv2.VideoWriter_fourcc(*'MJPG')
 
-cap.set(cv2.CAP_PROP_FOURCC, fourcc)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 3856)#widht of the image
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 2764)#heigth of the image
+cap.set(cv2.CAP_PROP_FORMAT, -1)#format of the video/photo
+cap.set(cv2.CAP_PROP_FPS, 7)#fps of the camera 
+cap.set(cv2.CAP_PROP_CONVERT_RGB, 1)#if something needs to be converted to rgb
+cap.set(cv2.CAP_PROP_FOURCC, fourcc)#define the codec
 
-cap.set(cv2.CAP_PROP_FORMAT, -1)
 
-cap.set(cv2.CAP_PROP_FPS, 7)
-
-cap.set(cv2.CAP_PROP_CONVERT_RGB, 1)
-
-result, image = cap.read()
-
+result, image = cap.read()#read a frame from the camera
 cv2.imshow('image', image)
 
 cv2.waitKey(0)
-
-cap.release()
+cap.release()#after reading a frame it releases the camera
 cv2.destroyAllWindows()
--- Lavoro_Stage_Pettenuzzo_Riccardo_2023/env/ImgRaw.py
+++ Lavoro_Stage_Pettenuzzo_Riccardo_2023/env/ImgRaw.py
@@ -3,7 +3,7 @@ import cv2
 
 stringa="C:\\Users\\stage.upe4\\Downloads\\raw\\IMG_0001_1.cr2"
 
-print(stringa.split(".")[-1])
+print(stringa.split(".")[-1])#split the path and take only the file type 
 raw = rawpy.imread(stringa) # access to the RAW image
 rgb = raw.postprocess() # a numpy RGB array
 image = cv2.cvtColor(rgb, cv2.COLOR_RGB2BGR) # the OpenCV image
